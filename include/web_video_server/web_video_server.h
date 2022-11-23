fab26e79e9caf8a808bc5c609d9533fb49f8cc3d
jwdinius <josephwdinius@gmail.com>
2022-11-23 09:28:52 -0800

    AAGSB-1944 make ecam_v4l2 service single camera feed only

--- include/web_video_server/web_video_server.h
+++ include/web_video_server/web_video_server.h
@@ -40,6 +40,8 @@ public:
 
   void setup_cleanup_inactive_streams();
 
+  int get_ros_threads() const;
+
   bool handle_stream(const async_web_server_cpp::HttpRequest &request,
                      async_web_server_cpp::HttpConnectionPtr connection, const char* begin, const char* end);
 
